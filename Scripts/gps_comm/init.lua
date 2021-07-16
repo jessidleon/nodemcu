332684efd68aa6203723cc34a36ced58829886d6
jessidleon <jessid.velez@protonmail.com>
2021-07-17 00:37:05 +0200

    added a push button verification(pin D5): if at 0, no execution. Useful to download data.txt

--- Scripts/gps_comm/init.lua
+++ Scripts/gps_comm/init.lua
@@ -1,11 +1,16 @@
-tmr.create():alarm(10000, tmr.ALARM_SINGLE, function()
-    dofile("log.lua").debug("started")
-    dofile("led.lua").do_initialization_blink()
-    file.close("led.lua")
-		
-    dofile("test_button.lua")
-    file.close("test_button.lua")
+tmr.create():alarm(3000, tmr.ALARM_SINGLE, function()
+	dofile("log.lua").debug("started")
+	dofile("led.lua").do_initialization_blink()
+	file.close("led.lua")
 
-    dofile("uart_0.lua")
-    file.close("uart_0.lua")
+	dofile("test_button.lua")
+	file.close("test_button.lua")
+
+	tmr.create():alarm(3000, tmr.ALARM_SINGLE, function()
+		print("continue?:", continue_execution)
+		if continue_execution then
+			dofile("uart_0.lua")
+			file.close("uart_0.lua")
+		end
+	end)
 end)
--- Scripts/gps_comm/test_button.lua
+++ Scripts/gps_comm/test_button.lua
@@ -1,36 +1,58 @@
-local input_pin = 3
+local delete_pin = 3
+local do_not_execute_pin = 5
 local led_pin = 4
+continue_execution = true
 
 local function setup_pins()
-    gpio.mode(input_pin, gpio.INPUT)
-    gpio.mode(led_pin, gpio.OUTPUT)
-    gpio.write(led_pin, gpio.HIGH)
+	gpio.mode(delete_pin, gpio.INPUT)
+	gpio.mode(do_not_execute_pin, gpio.INPUT)
+	gpio.mode(led_pin, gpio.OUTPUT)
+	gpio.write(led_pin, gpio.HIGH)
 end
 
-local function test_pin()
-    local pushed = true
-    local released = false
-    local value = gpio.read(input_pin)
-    if value == gpio.LOW then
-        return pushed
-    end
-    return released
+local function test_pin(pin_to_test)
+	local pushed = true
+	local released = false
+	local value = gpio.read(pin_to_test)
+	if value == gpio.LOW then
+		return pushed
+	end
+	return released
 end
 
 local function do_job()
-    local is_pushed = test_pin()
-    if is_pushed then
-        local initial_timer = tmr.create():alarm(1000, tmr.ALARM_SINGLE, function()
-            is_pushed = test_pin()
-            if is_pushed then
-                gpio.write(led_pin, gpio.LOW)
-                local file_to_remove = "data.txt"
-                file.remove(file_to_remove)
-                print("removed " .. file_to_remove)
-            end
-        end)
-    end
+	local is_pushed = test_pin(delete_pin)
+	if is_pushed then
+		tmr.create():alarm(2000, tmr.ALARM_SINGLE, function()
+			is_pushed = test_pin(delete_pin)
+			if is_pushed then
+				print("delete_pin is pushed")
+				gpio.write(led_pin, gpio.LOW)
+				local file_to_remove = "data.txt"
+				file.remove(file_to_remove)
+				print("removed " .. file_to_remove)
+				continue_execution = false
+			end
+		end)
+			else
+		print("delete_pin is released")
+	end
+
+	is_pushed = test_pin(do_not_execute_pin)
+	if is_pushed then
+		tmr.create():alarm(1000, tmr.ALARM_SINGLE, function()
+			is_pushed = test_pin(do_not_execute_pin)
+			if is_pushed then
+				gpio.write(led_pin, gpio.LOW)
+				print("do_not_execute_pin is pushed")
+				continue_execution = false
+			end
+		end)
+	else
+		print("do_not_execute_pin is released")
+	end
 end
 
 setup_pins()
 do_job()
+
